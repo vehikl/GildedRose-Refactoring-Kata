4ebbb3bb417919ce74b56ae144f4388b8e053c76
Colin DeCarlo <colin@thedecarlos.ca>
2019-05-02 09:16:31 -0500

    add snaps and get 100% coverage

--- js-jest/__tests__/gilded_rose_spec.js
+++ js-jest/__tests__/gilded_rose_spec.js
@@ -1,10 +1,33 @@
 var {Shop, Item} = require('../src/gilded_rose.js');
+const _ = require('lodash');
+
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+  const names = [
+    'foo',
+    'Aged Brie',
+    'Backstage passes to a TAFKAL80ETC concert',
+    'Sulfuras, Hand of Ragnaros'
+  ];
+
+  const sellIns = [-1, 0, 1, 5, 6, 7, 10, 11, 12];
+
+  const qualities = [0, 1, 49, 50, 51];
+
+  function cartesianProductOf() {
+    return _.reduce(arguments, function(a, b) {
+        return _.flatten(_.map(a, function(x) {
+            return _.map(b, function(y) {
+                return x.concat([y]);
+            });
+        }), true);
+    }, [ [] ]);
+  }
+
+  it.each(cartesianProductOf(names, sellIns, qualities))("should foo", function(name, sellIn, quality) {
+    const gildedRose = new Shop([ new Item(name, sellIn, quality) ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0]).toMatchSnapshot();
   });
 
 });
