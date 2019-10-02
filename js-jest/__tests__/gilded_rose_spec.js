2fa9bdb3559178e37fa1efb9d3ed1c42427e5e11
Vehikl Morning Mob <39924126+vehikl-morning-mob@users.noreply.github.com>
2019-10-02 15:13:34 -0400

    Add tests

--- js-jest/__tests__/gilded_rose_spec.js
+++ js-jest/__tests__/gilded_rose_spec.js
@@ -1,10 +1,25 @@
-var {Shop, Item} = require('../src/gilded_rose.js');
-describe("Gilded Rose", function() {
+import _ from 'lodash';
+import { Shop, Item } from '../../src/gilded_rose';
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+describe('Gilded Rose', () => {
+  const names = [
+    'foo',
+    'Aged Brie',
+    'Backstage passes to a TAFKAL80ETC concert',
+    'Sulfuras, Hand of Ragnaros',
+  ];
+
+  const sellIns = [-1, 0, 1, 5, 6, 7, 10, 11, 12];
+
+  const qualities = [0, 1, 49, 50, 51];
+
+  function cartesianProductOf() {
+    return _.reduce(arguments, (a, b) => _.flatten(_.map(a, x => _.map(b, y => x.concat([y]))), true), [[]]);
+  }
+
+  it.each(cartesianProductOf(names, sellIns, qualities))('should foo', (name, sellIn, quality) => {
+    const gildedRose = new Shop([new Item(name, sellIn, quality)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0]).toMatchSnapshot();
   });
-
 });
